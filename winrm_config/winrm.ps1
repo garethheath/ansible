c0db601cb6b00b22c1f08072a4ce8adc2d260538
Gareth Heath <gareth.heath@hmcts.net>
2020-11-11 12:14:10 +0000

    update code

---
+++ winrm_config/winrm.ps1
@@ -0,0 +1,12 @@
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+winrm enumerate winrm/config/Listener
+
+winrm get winrm/config/Service
+
+winrm get winrm/config/Winrs
